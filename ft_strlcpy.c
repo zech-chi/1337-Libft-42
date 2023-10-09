df73cfb6f8c2518b03b3e225b8c48cb9eab2f650
zakaria0101echifaouy <machinelearning1.618@gmail.com>
2023-10-09 20:13:46 +0100

    modified

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -13,42 +13,66 @@ size_t ft_strlen(const char *s)
 }
 
 
-size_t	strlcpy(char *dest, const char *src, size_t size)
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 {
-    
-}
+    size_t i;
+	size_t src_len;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-size_t	ft_strlcpy2(char *dest, const char *src, size_t size)
-{
-	size_t	i;
-
+	src_len = ft_strlen(src);
+	if (size == 0)
+		return src_len;
 	i = 0;
-	if (size)
+	while (i < size - 1 && src[i])
 	{
-		while (src[i] && i < size - 1)
-		{
-			dest[i] = src[i];
-			i++;
-		}
-		dest[i] = 0;
-	}
-	while (src[i])
+		dest[i] = src[i];
 		i++;
-	return (i);
+	}
+	dest[i] = '\0';
+	return (src_len);
 }
+
+// size_t	ft_strlcpy2(char *dest, const char *src, size_t size)
+// {
+// 	size_t	i;
+
+// 	i = 0;
+// 	if (size)
+// 	{
+// 		while (src[i] && i < size - 1)
+// 		{
+// 			dest[i] = src[i];
+// 			i++;
+// 		}
+// 		dest[i] = 0;
+// 	}
+// 	while (src[i])
+// 		i++;
+// 	return (i);
+// }
+
+// int main(int ac, char **av)
+// {
+// 	if (ac != 3)
+// 		return (0);
+// 	size_t size = atoi(av[1]);
+// 	char dest1[size];
+// 	char dest2[size];
+// 	char *src = av[2];
+
+// 	printf("size of dest: %ld\n", sizeof(dest1));
+// 	printf("src = \"%s\"\n\n", src);
+
+// 	// your output:
+// 	printf("your output: \n");
+// 	size_t res1 = ft_strlcpy(dest1, src, sizeof(dest1));
+// 	printf("dest = \"%s\"\n", dest1);
+// 	printf("returned : %ld\n", res1);
+// 	printf("\n");
+// 	// expected output:
+// 	printf("expected output\n");
+// 	size_t res2 = ft_strlcpy2(dest2, src, sizeof(dest2));
+// 	printf("dest = \"%s\"\n", dest2);
+// 	printf("returned : %ld\n", res2);
+	
+// 	return (0);
+// }
