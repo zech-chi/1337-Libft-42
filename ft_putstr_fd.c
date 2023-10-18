b27bddf2c25b3496a20264eb7085817dc31cac40
zakaria0101echifaouy <machinelearning1.618@gmail.com>
2023-10-18 18:22:34 +0100

    add ft_putstr_fd.c

---
+++ ft_putstr_fd.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+void ft_putchar_fd(char c, int fd)
+{
+    write(fd, &c, 1);
+}
+
+void ft_putstr_fd(char *s, int fd)
+{
+    while (*s)
+    {
+        ft_putchar_fd(*s, fd);
+        s++;
+    }
+}
