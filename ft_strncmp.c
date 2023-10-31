45d3178382c2b2bc11f79b43dd22a9244e23f4d2
Zakaria Ech-chifaouy <machinelearning1.618@gmail.com>
2023-10-31 21:00:28 +0100

    1337

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -1,28 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: zech-chi <zech-chi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/30 14:52:28 by zech-chi          #+#    #+#             */
-/*   Updated: 2023/10/31 20:42:57 by zech-chi         ###   ########.fr       */
+/*   Created: 2023/10/31 20:37:40 by zech-chi          #+#    #+#             */
+/*   Updated: 2023/10/31 20:54:49 by zech-chi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# include <stdlib.h>
-/* FUNCTIONS */
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
-int		ft_atoi(const char *str);
-int		ft_tolower(int c);
-int		ft_toupper(int c);
-size_t	ft_strlen(const char *s);
-int		ft_isprint(int c);
-int		ft_isascii(int c);
-int		ft_isalnum(int c);
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
+#include "libft.h"
 
-#endif
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	if (n == 0)
+		return (0);
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i] && i < n - 1)
+		i++;
+	return (s1[i] - s2[i]);
+}
