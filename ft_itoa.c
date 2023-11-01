f0465bd49089ff0af4c89c01d86a55aceea71851
Zakaria Ech-chifaouy <machinelearning1.618@gmail.com>
2023-11-01 23:26:31 +0100

    1337

---
+++ ft_itoa.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zech-chi <zech-chi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/01 22:55:04 by zech-chi          #+#    #+#             */
+/*   Updated: 2023/11/01 23:20:58 by zech-chi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	len_of_number(long long n)
+{
+	int	len;
+
+	len = 0;
+	if (n == 0)
+		return (1);
+	if (n < 0)
+	{
+		n *= -1;
+		len += 1;
+	}
+	while (n)
+	{
+		len += 1;
+		n /= 10;
+	}
+	return (len);
+}
+
+char	*fill(char *ptr, long long ln, int size)
+{
+	int	i;
+
+	if (ln == 0)
+	{
+		ptr[0] = '0';
+		ptr[1] = 0;
+		return (ptr);
+	}
+	if (ln < 0)
+	{
+		ptr[0] = '-';
+		ln *= -1;
+	}
+	i = size - 1;
+	while (ln)
+	{
+		ptr[i] = '0' + ln % 10;
+		ln /= 10;
+		i--;
+	}
+	ptr[size] = 0;
+	return (ptr);
+}
+
+char	*ft_itoa(int n)
+{
+	int			size;
+	long long	ln;
+	char		*ptr;
+
+	ln = (long long)n;
+	size = len_of_number(ln);
+	ptr = malloc(size + 1);
+	if (ptr == NULL)
+		return (NULL);
+	fill(ptr, ln, size);
+	return (ptr);
+}
