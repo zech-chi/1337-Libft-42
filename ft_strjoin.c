a84dbee237e32b28559f405642e4d6c9cc968945
Zakaria Ech-chifaouy <machinelearning1.618@gmail.com>
2023-11-01 22:54:41 +0100

    1337

---
+++ ft_strjoin.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zech-chi <zech-chi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/01 22:37:36 by zech-chi          #+#    #+#             */
+/*   Updated: 2023/11/01 22:54:14 by zech-chi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	size;
+	char	*ptr;
+
+	size = ft_strlen(s1) + ft_strlen(s2);
+	ptr = malloc(size + 1);
+	if (ptr == NULL)
+		return (NULL);
+	ft_strlcat(ptr, s1, size + 1);
+	ft_strlcat(ptr, s2, size + 1);
+	return (ptr);
+}
---
+++ ft_substr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zech-chi <zech-chi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/01 22:21:45 by zech-chi          #+#    #+#             */
+/*   Updated: 2023/11/01 22:38:49 by zech-chi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	size;
+	size_t	i;
+	char	*ptr;
+
+	size = ft_strlen(s) - start;
+	if (len < size)
+		size = len;
+	ptr = malloc((size + 1));
+	if (ptr == NULL)
+		return (NULL);
+	i = 0;
+	while (i < size)
+	{
+		ptr[i] = s[start + i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
