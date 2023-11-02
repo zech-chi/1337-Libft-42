b66d9ce28a5c31c82e5b2d503718cbee6c4cf179
Zakaria Ech-chifaouy <machinelearning1.618@gmail.com>
2023-11-02 15:12:51 +0100

    1337

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,30 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: zech-chi <zech-chi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/01 08:27:18 by zech-chi          #+#    #+#             */
-/*   Updated: 2023/11/02 14:29:10 by zech-chi         ###   ########.fr       */
+/*   Created: 2023/11/02 14:25:49 by zech-chi          #+#    #+#             */
+/*   Updated: 2023/11/02 15:12:10 by zech-chi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-//#include <stdio.h>
-//#include <string.h>
+#include <stdio.h>
+#include <string.h>
 
-void	*ft_memset(void *b, int c, size_t len)
+void	ft_bzero(void *s, size_t n)
 {
-	char	*str;
-	size_t	i;
-
-	str = (char *)b;
-	i = 0;
-	while (i < len)
-		str[i++] = (unsigned char)c;
-	return (b);
+	ft_memset(s, 0, n);
 }
+
 //int main(int ac, char **av)
 //{
 //	int arr1[] = {1, 2, 3, 4, 5, 6, 7};
@@ -36,8 +30,8 @@ void	*ft_memset(void *b, int c, size_t len)
 //		printf("%d, ", arr1[i]);
 //	printf("\n");
 
-//	ft_memset(arr1, 0, 4 * sizeof(arr1[0]));
-//	memset(arr2, 0, 4 * sizeof(arr2[0]));
+//	ft_bzero(arr1, sizeof(arr1));
+//	bzero(arr2, sizeof(arr2));
 	
 //	printf("------ my output ----\n");
 //	for (int i = 0; i < 7; i++)
@@ -46,6 +40,24 @@ void	*ft_memset(void *b, int c, size_t len)
 //	printf("------ expected -----\n");
 //	for (int i = 0; i < 7; i++)
 //		printf("%d, ", arr1[i]);
+//	printf("\n");
+
+
+//	char str1[] = "hello world";
+//	char str2[] = "hello world";
+
+//	printf("------ test2 ------\n");
+//	printf("%s", str1);
+//	printf("\n");
+
+//	ft_bzero(str1, sizeof(str1));
+//	bzero(str2, sizeof(str2));
+	
+//	printf("------ my output ----\n");
+//	printf("%s", str1);;
+//	printf("\n");
+//	printf("------ expected -----\n");
+//	printf("%s", str2);
 //	printf("\n");
 //	return (0);
 //}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: zech-chi <zech-chi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 14:52:28 by zech-chi          #+#    #+#             */
-/*   Updated: 2023/11/02 11:42:56 by zech-chi         ###   ########.fr       */
+/*   Updated: 2023/11/02 15:12:21 by zech-chi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 # define LIBFT_H
 # include <stdlib.h>
 /* FUNCTIONS */
+void	ft_bzero(void *s, size_t n);
+void	*ft_memset(void *b, int c, size_t len);
 char	**ft_split(char const *s, char c);
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_strjoin(char const *s1, char const *s2);
