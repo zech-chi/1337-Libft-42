57453fb8ee963e78b9e6044c34d9690c10920ea5
Zakaria Ech-chifaouy <machinelearning1.618@gmail.com>
2023-11-03 18:28:40 +0100

    1337

---
+++ ft_striteri.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zech-chi <zech-chi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/03 18:22:14 by zech-chi          #+#    #+#             */
+/*   Updated: 2023/11/03 18:28:11 by zech-chi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+	{
+		(*f)(i, &s[i]);
+		i++;
+	}
+}
+
+//void	change(unsigned int i, char *c)
+//{
+//	*c += i;
+//}
+
+//int main()
+//{
+//	char str[] = "HELLO";
+//	void (*f)(unsigned int, char *);
+//	f = change;
+//	ft_striteri(str, f);
+//	printf("%s\n", str);
+//	return (0);
+//}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: zech-chi <zech-chi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 14:52:28 by zech-chi          #+#    #+#             */
-/*   Updated: 2023/11/03 12:25:16 by zech-chi         ###   ########.fr       */
+/*   Updated: 2023/11/03 18:16:23 by zech-chi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 # include <stdlib.h>
 # include <unistd.h>
 /* FUNCTIONS */
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_itoa(int n);
 void	ft_putnbr_fd(int n, int fd);
