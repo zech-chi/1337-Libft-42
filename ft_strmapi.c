e07106c5a36db3d209413001491b6e51ccc35a7e
Zakaria Ech-chifaouy <machinelearning1.618@gmail.com>
2023-11-03 18:14:38 +0100

    1337

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,22 +6,46 @@
 /*   By: zech-chi <zech-chi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 14:52:15 by zech-chi          #+#    #+#             */
-/*   Updated: 2023/11/03 16:00:40 by zech-chi         ###   ########.fr       */
+/*   Updated: 2023/11/03 18:14:06 by zech-chi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
 	int		size;
-	
+	size_t	i;
 	char	*ptr;
 
 	size = ft_strlen(s);
 	ptr = malloc(size + 1);
 	if (ptr == NULL)
-		retrun (NULL);
-		
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		ptr[i] = (*f)(i, s[i]);
+		i++;
+	}
+	ptr[i] = 0;
+	return (ptr);
 }
+
+//char change(unsigned int i, char c)
+//{
+//	if ('a' <= c && c <= 'z')
+//		return (c - 32);
+//	return (c);
+//}
+
+//int main()
+//{
+//	char (*f)(unsigned int, char);
+//	f = change;
+//	char *ans = ft_strmapi("HEllO", f);
+//	printf("%s\n", ans);
+//	return (0);
+//}
