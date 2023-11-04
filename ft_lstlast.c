b02607456c3ba39b8c61da6df4c752ae928f87b6
Zakaria Ech-chifaouy <machinelearning1.618@gmail.com>
2023-11-04 23:55:09 +0100

    1337

---
+++ ft_lstlast.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstlast.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zech-chi <zech-chi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/04 23:49:47 by zech-chi          #+#    #+#             */
+/*   Updated: 2023/11/04 23:51:48 by zech-chi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	t_list	*cur_node;
+
+	cur_node = lst;
+	if (cur_node == NULL)
+		return (NULL);
+	while (cur_node->next)
+		cur_node = cur_node->next;
+	return (cur_node);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: zech-chi <zech-chi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 14:52:28 by zech-chi          #+#    #+#             */
-/*   Updated: 2023/11/04 23:35:54 by zech-chi         ###   ########.fr       */
+/*   Updated: 2023/11/04 23:52:03 by zech-chi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,6 +60,7 @@ typedef struct s_list
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int	ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
 
 
 
