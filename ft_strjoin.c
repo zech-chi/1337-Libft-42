8807fe133b67ab92fb99b751531addcc0deee525
Zakaria Ech-chifaouy <machinelearning1.618@gmail.com>
2023-11-05 01:50:37 +0100

    1337

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: zech-chi <zech-chi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 22:37:36 by zech-chi          #+#    #+#             */
-/*   Updated: 2023/11/04 19:13:17 by zech-chi         ###   ########.fr       */
+/*   Updated: 2023/11/05 01:49:50 by zech-chi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,23 @@ char	*ft_strjoin(char const *s1, char const *s2)
 {
 	size_t	size;
 	char	*ptr;
+	size_t	i;
+	size_t	j;
 
 	size = ft_strlen(s1) + ft_strlen(s2);
 	ptr = malloc(size + 1);
 	if (ptr == NULL)
 		return (NULL);
-	ft_strlcat(ptr, s1, size + 1);
-	ft_strlcat(ptr, s2, size + 1);
+	i = 0;
+	if (size > 0)
+	{
+		j = 0;
+		while (s1[j])
+			ptr[i++] = s1[j++];
+		j = 0;
+		while (s2[j])
+			ptr[i++] = s2[j++];
+	}
+	ptr[i] = 0;
 	return (ptr);
 }
