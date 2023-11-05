bca3aac2e44625dce4dfe3bee09cbc81e1da7eda
Zakaria Ech-chifaouy <machinelearning1.618@gmail.com>
2023-11-05 01:15:32 +0100

    1337

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: zech-chi <zech-chi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 21:53:31 by zech-chi          #+#    #+#             */
-/*   Updated: 2023/11/04 18:04:25 by zech-chi         ###   ########.fr       */
+/*   Updated: 2023/11/05 01:14:46 by zech-chi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ char	*ft_strchr(const char *s, int c)
 	char	*ptr;
 
 	ptr = (char *)s;
-	while (*ptr && *ptr != c)
+	while (*ptr && *ptr != (char)c)
 		ptr++;
-	if (*ptr == c)
+	if (*ptr == (char)c)
 		return (ptr);
 	return (0);
 }
